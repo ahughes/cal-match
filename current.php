665fc09362adf46c7d3e5b4f10ec35324f98c49a
Alexander Hughes <alexander.hughes@utah.edu>
2017-03-18 12:35:55 -0600

    Create nav-bar partial and restructured modal partials

--- current.php
+++ current.php
@@ -18,48 +18,8 @@
     <![endif]-->
   </head>
   <body>
-    <nav class="navbar navbar-toggleable-md navbar-inverse fixed-top bg-inverse">
-      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <a class="navbar-brand text-info" href="#">Calorie Match</a>
-
-      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home&nbsp;<span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">About</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Restaurants Near Me</a>
-            <div class="dropdown-menu" aria-labelledby="dropdown01">
-              <a class="dropdown-item" href="../restaurants/mcdonalds.php">McDonald's</a>
-              <a class="dropdown-item" href="../restaurants/wendys.php">Wendy's</a>
-              <a class="dropdown-item" href="../restaurants/burger-king.php">Burger&nbsp;King</a>
-            </div>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#" data-toggle="modal" data-target="#signUpModal">Sign&nbsp;Up</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#" data-toggle="modal" data-target="#contactUs">Contact&nbsp;Us</a>
-          </li>
-          <!-- <li class="nav-item">
-            <a class="nav-link" href="#">Sign In</a>
-          </li> -->
-          <!-- <li class="nav-item">
-            <a class="nav-link disabled" href="#">Sign Out</a>
-          </li> -->
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="text" placeholder="Email">
-          <input class="form-control mr-sm-2" type="password" placeholder="Password">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Sign&nbsp;In</button>
-        </form>
-      </div>
-    </nav>
+    <!-- Include the navigation bar -->
+    <? include_once('partials/nav.html'); ?>
 
     <!-- Main jumbotron for a primary marketing message or call to action -->
     <div class="jumbotron">
@@ -140,10 +100,10 @@
       </footer>
     </div> <!-- /container -->
     
-    <!-- Include partials -->
+    <!-- Include modals -->
     <?php
-      include_once('partials/contactUsModal.php');
-      include_once('partials/signUpModal.php');
+      include_once('partials/contactUsModal.html');
+      include_once('partials/signUpModal.html');
     ?>
 
     <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
