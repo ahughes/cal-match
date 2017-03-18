214368a3527eea43405f33413af9024687ecd4af
alasii <alasii@hotmail.com>
2017-03-18 09:16:21 -0600

    Cart Page
    Cart Page Layout and DB connection

--- alacurrentCart.php
+++ alacurrentCart.php
@@ -1,157 +1,152 @@
-<!doctype html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Calorie Match</title>
-    <meta name="description" content="IS4460 Project by Group 19">
-    <meta name="author" content="Ala Brown, Alexander Hughes, and David Houghton">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE HTML>
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/main.css">
-    <!--[if lt IE 9]>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
-        <script>window.html5 || document.write('<script src="js/html5shiv.js"><\/script>')</script>
-    <![endif]-->
-  </head>
-  <body>
-    <nav class="navbar navbar-toggleable-md navbar-inverse fixed-top bg-inverse">
-      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <a class="navbar-brand text-info" href="#">Calorie Match</a>
+<HEADER><h2>Cart</h2></HEADER>
+<HEAD>
 
-      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home&nbsp;<span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">About</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Restaurants Near Me</a>
-            <div class="dropdown-menu" aria-labelledby="dropdown01">
-              <a class="dropdown-item" href="../restaurants/mcdonalds.php">McDonald's</a>
-              <a class="dropdown-item" href="../restaurants/wendys.php">Wendy's</a>
-              <a class="dropdown-item" href="../restaurants/burger-king.php">Burger&nbsp;King</a>
-            </div>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#" data-toggle="modal" data-target="#signUpModal">Sign&nbsp;Up</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#" data-toggle="modal" data-target="#contactUs">Contact&nbsp;Us</a>
-          </li>
-          <!-- <li class="nav-item">
-            <a class="nav-link" href="#">Sign In</a>
-          </li> -->
-          <!-- <li class="nav-item">
-            <a class="nav-link disabled" href="#">Sign Out</a>
-          </li> -->
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="text" placeholder="Email">
-          <input class="form-control mr-sm-2" type="password" placeholder="Password">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Sign&nbsp;In</button>
-          <button href="#" class="btn btn-outline-secondary ml-2 my-2 my-sm-0"><i class="fa fa-shopping-cart"></i></button>
-        </form>
-      </div>
-    </nav>
+</HEAD>
+<BODY>
+  <DIV>
+<TABLE width="60%" style="border: 1px solid black">
+    <TR>
+    <TD style="border-bottom:1px solid black" width="65%">
+      <LABEL>Item</LABEL>
+    </TD>
+    <TD style="border-bottom:1px solid black" width="15%">
+      <LABEL>Restaurant</LABEL>
+    </TD>
+    <TD style="border-bottom:1px solid black" width="10%">
+      <LABEL>Price</LABEL>
+    </TD>
+    <TD style="border-bottom:1px solid black" width="10%">
+      <LABEL>Calories</LABEL>
+    </TD>
+    <TD width="10%">
+      <IMG SRC="img/remove.png" width="40px">
+    </TD>
 
-    <!-- Main jumbotron for a primary marketing message or call to action -->
-    <div class="jumbotron">
-      <div class="container">
-        <h1 class="display-3">Hello, world!</h1>
-        <p>Welcome to Calorie Match! This is being developed as part of a group project for IS 4460. It's soon going to be an awesome web application to help track calorie consumption by matching "available calories" with menu items from fast food restaurants.</p>
-        <p>Want to give it a shot? Simply enter an amount of calories in the box below, and you will be shown all the possible food items with less than or equal to the amount of calories provided.</p>
-        <hr>
-        <p>Have an awesome idea for our project? Found a bug? Want to give us a thumbs up? <a class="btn btn-primary btn-sm" href="#" role="button" data-toggle="modal" data-target="#contactUs">Contact us &raquo;</a></p>
-      </div>
-    </div>
+  </TR>
 
-    <div class="container">
-      <!-- Search by item name -->
-      <div class="row justify-content-sm-center">
-        <div class="col-12 col-xs-auto mb-3">Search available items:</div>
-      </div>
-      <div class="row justify-content-sm-center">
-        <div class="col-12 col-xs-auto">
-          <form class="form-inline" method="POST">
-            <!-- <div class="form-group"> -->
-              <input type="text" onkeyup="suggest(this.value)">
-            <!-- </div> -->
-          </form>
-        </div>
-      </div>
-      <div class="row justify-content-sm-center">
-        <div class="col-12 col-xs-auto mb-3"><p>Suggestions: <span id="search_suggestions"></span></p></div>
-      </div>
+<?php
 
-      <!-- Search by number of calories -->
-      <div class="row justify-content-sm-center">
-        <div class="col-12 col-xs-auto mb-3">Enter available calories:</div>
-      </div>
-      <div class="row justify-content-sm-center">
-        <div class="col-12 col-xs-auto">
-          <form class="form-inline" method="POST">
-            <div class="form-group">
-              <input name="cal_value" type="text" class="form-control" placeholder="Example: 500" aria-describedby="Available Calories">
-              <span class="input-group-addon">calories</span>
-            </div>
-            <button type="submit" class="btn btn-primary mx-3" id="cal_val_submit">Submit</button>
-          </form>
-        </div>
-      </div>
+include_once('config/db_functions.php');
 
-      <br><br>
+$query1 = "select * from cart c
+          inner join item i on i.itemID = c.itemID
+          inner join restaurant r on r.restaurantID = i.restaurantID
+          where c.cartID = 1";
 
-      <div class="row">
+$rows = db_select($query1);
+
+foreach($rows as $row) { echo '
+              
+  <TR>
+    <TD>
+      <LABEL>'.$row[name].'</LABEL>
+    </TD>
+    <TD>
+      <LABEL>'.$row[name].'</LABEL>
+    </TD>
+    <TD>
+      <LABEL>$ '.$row[cartID].'</LABEL>
+    </TD>
+    <TD>
+      <LABEL>'.$row[cartID].'</LABEL>
+    </TD>
+    <TD style="text-align:center">
+      <INPUT TYPE="BUTTON" NAME=cartID'.$row[0].' '.'itemID'.$row[1].' VALUE="X">
+    </TD>
+  </TR>';
+}
+
+
+
+
+
+
+/*
+$total = $row[6];
+$totalAmt += $total;
+
+$cal = $row[5];
+$totalCal += $cal;
+
+ echo'
+    <TR>
+    <TD>
+      <LABEL>'.$row[4].'</LABEL>
+    </TD>
+    <TD>
+      <LABEL>'.$row[8].'</LABEL>
+    </TD>
+    <TD>
+      <LABEL>$ '.$row[6].'</LABEL>
+    </TD>
+    <TD>
+      <LABEL>'.$row[5].'</LABEL>
+    </TD>
+    <TD style="text-align:center">
+      <INPUT TYPE="BUTTON" NAME=cartID'.$row[0].' '.'itemID'.$row[1].' VALUE="X">
+    </TD>
+  </TR>
+';
+}
+*/
+?>
+
+
+<TR>
+    <TD style="border-top:1px solid black">
+      <LABEL>State Food Tax Rate 4.71%</LABEL>
+    </TD>
+    <TD style="border-top:1px solid black">
+      <LABEL></LABEL>
+    </TD>
+    <TD style="border-top:1px solid black">
+      <LABEL>
+        <?php 
+        $tax=0.0471; 
+        $plusTax = money_format('%i',$totalAmt * $tax); 
+        echo '$ '.$plusTax; ?></LABEL>
+    </TD>
+    <TD style="border-top:1px solid black">
+    </TD>
+</TR>
+<TR>
+    <TD style="border-top:1px solid black">
+      <LABEL>TOTAL</LABEL>
+    </TD>
+    <TD style="border-top:1px solid black">
+      <LABEL></LABEL>
+    </TD>
+    <TD style="border-top:1px solid black">
+      <LABEL>
+        <?php 
+        $grandtotal = ($totalAmt * $tax)+$totalAmt;
+        $grandtotal = money_format('%i',$grandtotal);
+        echo "$ ".$grandtotal; ?></LABEL>
+    </TD>
+    <TD style="border-top:1px solid black">
+      <LABEL>
         <?php
-          if(isset($_POST['cal_value'])) {
-            $cal_value = $_POST['cal_value'];
-            $remaining = $cal_value - 10;
-            include_once('config/db_functions.php');
-            $query1 = "SELECT * FROM `item` WHERE `calories` <= $cal_value ORDER BY calories";
-            $rows = db_select($query1);
-            echo '
-              <div class="progress" style="width: 100vw;">
-                <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: ' . ($remaining/$cal_value)*100 . '%">' . $remaining .' calories remaining</div>
-              </div><br><br>';
-            echo '<div class="card-columns">';
-            foreach($rows as $row) { echo '
-              <div class="card text-center">
-                <img class="card-img-top mx-auto logo" style="max-width: 150px;" src="img/logo' . $row['restaurantID'] . '.png" alt="Item image">
-                <div class="card-block">
-                  <h4 class="card-title">' . $row['name'] . '</h4>
-                  <p class="card-text">' . $row['calories'] . ' calories  |  $' . $row['price'] . '</p>
-                  <p class="card-text"><a href="#" class="btn btn-primary" role="button">Order Now</a></p>
-                </div>
-              </div>';
-            }
-          }
+        echo $totalCal;
         ?>
-      </div>
+      </LABEL>
+    </TD>
+</TR>
 
-      <hr>
 
-      <footer>
-        <p>&copy; 2017 | Group 19 | IS4460</p>
-      </footer>
-    </div> <!-- /container -->
-    
-    <!-- Include partials -->
-    <?php
-      include_once('partials/contactUsModal.php');
-      include_once('partials/signUpModal.php');
-    ?>
+</TABLE>
+<BR/>
+<INPUT TYPE="BUTTON" VALUE="ADD MORE ITEMS">
+<INPUT TYPE="BUTTON" VALUE="CHECKOUT">
 
-    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
-    <script src="js/main.js"></script>
+</BODY>
 
-  </body>
-</html>
+
+</HTML>
+<style>
+  
+  div{
+    /*display:inline-block;*/
+  }
+</style>
