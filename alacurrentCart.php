0cbfb690037cbc99d13ffb6a62d1302f39da5527
alasii <alasii@hotmail.com>
2017-03-17 20:31:03 -0600

    Test add items to a cart

--- alacurrentCart.php
+++ alacurrentCart.php
@@ -8,6 +8,7 @@
     <meta name="author" content="Ala Brown, Alexander Hughes, and David Houghton">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
     <link rel="stylesheet" href="css/main.css">
     <!--[if lt IE 9]>
@@ -31,18 +32,18 @@
             <a class="nav-link" href="#">About</a>
           </li>
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Restaurants</a>
+            <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Restaurants Near Me</a>
             <div class="dropdown-menu" aria-labelledby="dropdown01">
-              <a class="dropdown-item" href="#">McDonald's</a>
-              <a class="dropdown-item" href="#">Wendy's</a>
-              <a class="dropdown-item" href="#">Burger&nbsp;King</a>
+              <a class="dropdown-item" href="../restaurants/mcdonalds.php">McDonald's</a>
+              <a class="dropdown-item" href="../restaurants/wendys.php">Wendy's</a>
+              <a class="dropdown-item" href="../restaurants/burger-king.php">Burger&nbsp;King</a>
             </div>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Sign&nbsp;Up</a>
+            <a class="nav-link" href="#" data-toggle="modal" data-target="#signUpModal">Sign&nbsp;Up</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Contact&nbsp;Us</a>
+            <a class="nav-link" href="#" data-toggle="modal" data-target="#contactUs">Contact&nbsp;Us</a>
           </li>
           <!-- <li class="nav-item">
             <a class="nav-link" href="#">Sign In</a>
@@ -52,9 +53,10 @@
           </li> -->
         </ul>
         <form class="form-inline my-2 my-lg-0">
-          <!-- <input class="form-control mr-sm-2" type="text" placeholder="Email"> -->
-          <!-- <input class="form-control mr-sm-2" type="password" placeholder="Password"> -->
+          <input class="form-control mr-sm-2" type="text" placeholder="Email">
+          <input class="form-control mr-sm-2" type="password" placeholder="Password">
           <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Sign&nbsp;In</button>
+          <button href="#" class="btn btn-outline-secondary ml-2 my-2 my-sm-0"><i class="fa fa-shopping-cart"></i></button>
         </form>
       </div>
     </nav>
@@ -64,13 +66,31 @@
       <div class="container">
         <h1 class="display-3">Hello, world!</h1>
         <p>Welcome to Calorie Match! This is being developed as part of a group project for IS 4460. It's soon going to be an awesome web application to help track calorie consumption by matching "available calories" with menu items from fast food restaurants.</p>
-        <p>Want to give it a shot? Simply enter an amount of calories in the box below, and you will be shown all the possible food items with less than or equal to the amount of calories provided. Testing for live push</p>
+        <p>Want to give it a shot? Simply enter an amount of calories in the box below, and you will be shown all the possible food items with less than or equal to the amount of calories provided.</p>
         <hr>
-        <p>Have an awesome idea for our project? Found a bug? Want to give us a thumbs up? <a class="btn btn-primary btn-sm" href="#" role="button">Contact us &raquo;</a></p>
+        <p>Have an awesome idea for our project? Found a bug? Want to give us a thumbs up? <a class="btn btn-primary btn-sm" href="#" role="button" data-toggle="modal" data-target="#contactUs">Contact us &raquo;</a></p>
       </div>
     </div>
 
     <div class="container">
+      <!-- Search by item name -->
+      <div class="row justify-content-sm-center">
+        <div class="col-12 col-xs-auto mb-3">Search available items:</div>
+      </div>
+      <div class="row justify-content-sm-center">
+        <div class="col-12 col-xs-auto">
+          <form class="form-inline" method="POST">
+            <!-- <div class="form-group"> -->
+              <input type="text" onkeyup="suggest(this.value)">
+            <!-- </div> -->
+          </form>
+        </div>
+      </div>
+      <div class="row justify-content-sm-center">
+        <div class="col-12 col-xs-auto mb-3"><p>Suggestions: <span id="search_suggestions"></span></p></div>
+      </div>
+
+      <!-- Search by number of calories -->
       <div class="row justify-content-sm-center">
         <div class="col-12 col-xs-auto mb-3">Enter available calories:</div>
       </div>
@@ -80,10 +100,8 @@
             <div class="form-group">
               <input name="cal_value" type="text" class="form-control" placeholder="Example: 500" aria-describedby="Available Calories">
               <span class="input-group-addon">calories</span>
-              <!-- <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> -->
             </div>
-
-            <button type="submit" class="btn btn-primary mx-3">Submit</button>
+            <button type="submit" class="btn btn-primary mx-3" id="cal_val_submit">Submit</button>
           </form>
         </div>
       </div>
@@ -94,13 +112,18 @@
         <?php
           if(isset($_POST['cal_value'])) {
             $cal_value = $_POST['cal_value'];
+            $remaining = $cal_value - 10;
             include_once('config/db_functions.php');
-            $query1 = "SELECT * FROM `item` WHERE `calories` <= $cal_value";
+            $query1 = "SELECT * FROM `item` WHERE `calories` <= $cal_value ORDER BY calories";
             $rows = db_select($query1);
+            echo '
+              <div class="progress" style="width: 100vw;">
+                <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: ' . ($remaining/$cal_value)*100 . '%">' . $remaining .' calories remaining</div>
+              </div><br><br>';
             echo '<div class="card-columns">';
             foreach($rows as $row) { echo '
               <div class="card text-center">
-                <img class="card-img-top mx-auto" style="max-width: 150px;" src="img/big-mac.png" alt="Item image">
+                <img class="card-img-top mx-auto logo" style="max-width: 150px;" src="img/logo' . $row['restaurantID'] . '.png" alt="Item image">
                 <div class="card-block">
                   <h4 class="card-title">' . $row['name'] . '</h4>
                   <p class="card-text">' . $row['calories'] . ' calories  |  $' . $row['price'] . '</p>
@@ -117,10 +140,18 @@
       <footer>
         <p>&copy; 2017 | Group 19 | IS4460</p>
       </footer>
-    </div> <!-- /container -->        
+    </div> <!-- /container -->
+    
+    <!-- Include partials -->
+    <?php
+      include_once('partials/contactUsModal.php');
+      include_once('partials/signUpModal.php');
+    ?>
+
     <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
     <script src="js/main.js"></script>
+
   </body>
 </html>
