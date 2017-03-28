1920a815196eba726806eb02b808ef1d224a5a17
alasii <alasii@hotmail.com>
2017-03-27 23:29:35 -0600

    add simple credit card input
    add simple credit card input
    Checkout Page

--- alacurrentCheckout.php
+++ alacurrentCheckout.php
@@ -27,11 +27,30 @@
       </TD>
     </TR>
 
-    <FORM>
-      
-    </FORM>
-
   </TABLE>
+
+  <DIV>
+    <FORM>
+      <DIV>
+      <SELECT>
+        <OPTION VALUE='1'>VISA</OPTION>
+        <OPTION VALUE='2'>AMERICAN EXPRESS</OPTION>
+        <OPTION VALUE='3'>MASTERCARD</OPTION>
+      </SELECT>
+    </DIV>
+    <DIV>
+      Credit Card Number <INPUT TYPE="TEXTBOX">
+    </DIV>
+      Expiration Date <INPUT TYPE="TEXTBOX">
+    </DIV>
+    <DIV>
+      Security Code <INPUT TYPE="TEXTBOX">      
+    </DIV>
+    <DIV>
+      Billing Zipcode <INPUT TYPE="TEXTBOX">  
+    </DIV>  
+    </FORM>
+  </DIV>
 
 
 
