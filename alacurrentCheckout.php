5dfee253ef8056e51665549ba0c810e3d2ab9091
alasii <alasii@hotmail.com>
2017-03-27 23:55:20 -0600

    add user info from database
    db connect info from user

--- alacurrentCheckout.php
+++ alacurrentCheckout.php
@@ -5,59 +5,139 @@
 </HEAD>
 <BODY>
 
-  <TABLE>
+<TABLE>
 
-    <TR>
-      <TD>
+<TR>
+
+  <TD>
+    <DIV>
+      Total Amount
+    </DIV>
+  </TD>
+
+  <TD>
+    <DIV>
+      <?php 
+      if(isset($_GET['totalAmt'])) 
+      {
+        $totalAmt = $_GET['totalAmt']; 
+        echo $totalAmt;
+      } 
+      ?>
+    </DIV>
+  </TD>
+
+</TR>
+
+
+<TR>
+
+  <TD>
+    <DIV>
+      First Name
+    </DIV>
+  </TD>
+
+  <TD>
+    <DIV>
+      <?php 
+      include_once('config/db_functions.php');
+      $query = "SELECT * FROM user WHERE userID = 1";
+
+      $rows = db_select($query);
+
+      $totalAmt = 0;
+      $totalCal = 0;
+
+      foreach($rows as $row){ 
+        echo'
+        '.$row[firstName].'
+    </DIV>
+  </TD>
+
+</TR>
+
+<TR>
+
+  <TD>
+    <DIV>
+      Last Name
+    </DIV>
+  </TD>
+
+  <TD>
+    <DIV>
+      '.$row[lastName].'
+    </DIV>
+  </TD>
+
+</TR>
+
+<TR>
+
+  <TD>
+    <DIV>
+      Phone Number
+    </DIV>
+  </TD>
+
+  <TD>
+    <DIV>
+      '.$row[phone].'
+    </DIV>
+  </TD>
+
+</TR>
+
+<TR>
+
+  <TD>
+    <DIV>
+      Email
+    </DIV>
+  </TD>
+
+  <TD>
+    <DIV>
+      '.$row[email].'
+    </DIV>
+  </TD>
+
+</TR>';
+      
+}
+?>
+</TABLE>
+
+    <DIV>
+      <FORM>
         <DIV>
-          Total Amount
+          Select Credit Card Type
+          <SELECT>
+            <OPTION VALUE='1'>VISA</OPTION>
+            <OPTION VALUE='2'>AMERICAN EXPRESS</OPTION>
+            <OPTION VALUE='3'>MASTERCARD</OPTION>
+          </SELECT>
         </DIV>
-      </TD>
-
-      <TD>
         <DIV>
-          <?php 
-          if(isset($_GET['totalAmt'])) 
-            {
-              $totalAmt = $_GET['totalAmt']; 
-              echo $totalAmt;
-            } 
-            ?>
+          Credit Card Number <INPUT TYPE="TEXTBOX">
         </DIV>
-      </TD>
-    </TR>
-
-  </TABLE>
-
-  <DIV>
-    <FORM>
+        Expiration Date <INPUT TYPE="TEXTBOX">
+      </DIV>
       <DIV>
-        Select Credit Card Type
-      <SELECT>
-        <OPTION VALUE='1'>VISA</OPTION>
-        <OPTION VALUE='2'>AMERICAN EXPRESS</OPTION>
-        <OPTION VALUE='3'>MASTERCARD</OPTION>
-      </SELECT>
-    </DIV>
-    <DIV>
-      Credit Card Number <INPUT TYPE="TEXTBOX">
-    </DIV>
-      Expiration Date <INPUT TYPE="TEXTBOX">
-    </DIV>
-    <DIV>
-      Security Code <INPUT TYPE="TEXTBOX">      
-    </DIV>
-    <DIV>
-      Billing Zipcode <INPUT TYPE="TEXTBOX">  
-    </DIV>  
+        Security Code <INPUT TYPE="TEXTBOX">      
+      </DIV>
+      <DIV>
+        Billing Zipcode <INPUT TYPE="TEXTBOX">  
+      </DIV>  
     </FORM>
   </DIV>
+  <INPUT TYPE="BUTTON" VALUE="PURCHASE">
 
 
 
 
-
-</BODY>
+  </BODY>
 
 
 </HTML>
